21c78ff497f67b5cce95de9a7904c3e37d7b320c
Hirad Roshandel <p30lord.hirad@yahoo.com>
2015-06-09 16:44:16 -0700

    newsfeed post

--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,12 @@ module.exports=function(app,Parse) {
       res.render('index', {title: 'The index page!'})
     });
 
+
+    /*******************************************
+     *
+     * NEWS FEED
+     *
+     ********************************************/
   app.get('/newsfeed', function (req, res, next) {
       var currentUser = Parse.User.current();
       if (currentUser) {
@@ -21,7 +27,32 @@ module.exports=function(app,Parse) {
       }
 
 });
+    app.post('/newsfeed', function (req, res, next) {
+        var currentUser = Parse.User.current();
+        if (currentUser) {
+            var NewsFeed = Parse.Object.extend("NewsFeed");
+            var newsFeed= new NewsFeed();
+            newsFeed.set('from',currentUser);
+            newsFeed.set('content',req.body.content);
+            newsFeed .save(null, {
+                success: function(newsfeed) {
+                    // Execute any logic that should take place after the object is saved.
+                    res.render('newsfeed', {title: 'NewsFeed', username: currentUser.attributes.username});
+                },
+                error: function(newsfeed, error) {
+                    // Execute any logic that should take place if the save fails.
+                    // error is a Parse.Error with an error code and message.
+                    alert('Failed to create new object, with error code: ' + error.message);
+                    res.render('newsfeed', {title: 'NewsFeed', msg: 'Posting content failed!'});
+                }
+            });
 
+
+        } else {
+            res.render('guest', {title: 'Login failed'});
+        }
+
+    });
 
   /*******************************************
    *
