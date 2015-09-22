cb8a3eae020d2cdc33d71390d5a5f14932c6b996
theodorelau <Theodore Lau>
2015-09-22 15:42:17 -0700

    New Publications Form on Profile Page

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -12,8 +12,8 @@ var OrganizationMenu = React.createClass ({
             <div id="tabs">
                 <ul id="content-nav">
                     {this.props.tabs.map(function(tab,index){
-                    var style = ""
                     if (self.state.focused == index) {
+                    var style = ""
                         style = "selected-tab";
                     }
                     return <li id={style}>
@@ -23,16 +23,70 @@ var OrganizationMenu = React.createClass ({
                 </ul>
             </div>
             <div id="content" className="content">
-                Selected: {this.props.tabs[this.state.focused]}
+                {self.state.focused == 0 ? <Connections/> : "" }
+                {self.state.focused == 1 ? <People/> : "" }
+                {self.state.focused == 2 ? <About/> : "" }
+                {self.state.focused == 3 ? <NewsAndEvents/> : "" }
+                {self.state.focused == 4 ? <Knowledge/> : "" }
             </div>
             </div>
         );
     }
 });
 
+var Connections = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Connections of {objectId}
+      </div>
+    )
+  }
+});
+
+var People = React.createClass({
+  render: function() {
+    return (
+      <div>
+        People
+      </div>
+    )
+  }
+});
+
+var About = React.createClass({
+mixins: [ParseReact.Mixin],
+  render: function() {
+    return (
+      <div>
+        About
+      </div>
+    )
+  }
+});
+
+var NewsAndEvents = React.createClass({
+  render: function() {
+    return (
+      <div>
+        NewsAndEvents
+      </div>
+    )
+  }
+});
+
+var Knowledge = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Knowledge
+      </div>
+    )
+  }
+});
+
 React.render(
     <OrganizationMenu tabs={['Connections', 'People', 'About', 'News And Events', 'Knowledge']}/>,
     document.getElementById('item-bottom-2-organization'));
-
 
 
