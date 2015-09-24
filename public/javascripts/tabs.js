62cb23ae2f56a3cbdebb360da682151e49800dc3
lisa <li.mw.lisa@gmail.com>
2015-09-24 10:26:41 -0700

    ported organization view to React.js
    currently, all content variables are passed to javascript like how they
    are in newsfeed.js (made global through the intermediary handlebars
    template). Will investigate a better method to do this

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -1,3 +1,26 @@
+var Organization = React.createClass ({
+    render: function() {
+        return (
+            <div>
+                <div className="item-top item-top-container">
+                    <div className="item-top-1 col">
+                        <img src={organization_imgURL} className="contain-image" />
+                    </div>
+                </div>
+                <div className="item-bottom">
+                    <div className="item-bottom-1">
+                        <div className="item-panel contain-panel" id="item-name"><h4>{name}</h4></div>
+                        <div className="item-panel contain-panel" id="item-location"><h4>{orgLocation}</h4></div>
+                    </div>
+                    <div id="item-bottom-2-organization" className="item-bottom-2">
+                        <OrganizationMenu tabs={['Connections', 'People', 'About', 'News And Events', 'Knowledge']} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
 var OrganizationMenu = React.createClass ({
     getInitialState: function() {
         return { focused: 0 };
@@ -7,28 +30,29 @@ var OrganizationMenu = React.createClass ({
     },
     render: function() {
         var self = this;
+        var tabMap = {0: <Connections />,
+                1: <People />,
+                2: <About />,
+                3: <NewsAndEvents/>,
+                4: <Knowledge/>};
         return (
             <div>
-            <div id="tabs">
-                <ul id="content-nav">
-                    {this.props.tabs.map(function(tab,index){
-                    if (self.state.focused == index) {
-                    var style = ""
-                        style = "selected-tab";
-                    }
-                    return <li id={style}>
-                            <a href="#" onClick={self.clicked.bind(self, index)} id={style}>{tab}</a>
-                           </li>;
-                    })}
-                </ul>
-            </div>
-            <div id="content" className="content">
-                {self.state.focused == 0 ? <Connections/> : "" }
-                {self.state.focused == 1 ? <People/> : "" }
-                {self.state.focused == 2 ? <About/> : "" }
-                {self.state.focused == 3 ? <NewsAndEvents/> : "" }
-                {self.state.focused == 4 ? <Knowledge/> : "" }
-            </div>
+                <div id="tabs">
+                    <ul id="content-nav">
+                        {this.props.tabs.map(function(tab,index){
+                            var style = "";
+                            if (self.state.focused == index) {
+                                style = "selected-tab";
+                            }
+                        return <li id={style}>
+                                <a href="#" onClick={self.clicked.bind(self, index)} id={style}>{tab}</a>
+                               </li>;
+                        })}
+                    </ul>
+                </div>
+                <div id="content" className="content">
+                    {tabMap.hasOwnProperty(self.state.focused) ? tabMap[self.state.focused] : ""}
+                </div>
             </div>
         );
     }
@@ -38,7 +62,7 @@ var Connections = React.createClass({
   render: function() {
     return (
       <div>
-        Connections of {objectId}
+        Connections of {name}
       </div>
     )
   }
@@ -48,18 +72,17 @@ var People = React.createClass({
   render: function() {
     return (
       <div>
-        People
+        People of {name}
       </div>
     )
   }
 });
 
 var About = React.createClass({
-mixins: [ParseReact.Mixin],
   render: function() {
     return (
       <div>
-        About
+        {about}
       </div>
     )
   }
@@ -69,7 +92,7 @@ var NewsAndEvents = React.createClass({
   render: function() {
     return (
       <div>
-        NewsAndEvents
+        News And Events
       </div>
     )
   }
@@ -85,8 +108,4 @@ var Knowledge = React.createClass({
   }
 });
 
-React.render(
-    <OrganizationMenu tabs={['Connections', 'People', 'About', 'News And Events', 'Knowledge']}/>,
-    document.getElementById('item-bottom-2-organization'));
-
-
+React.render(<Organization />, document.getElementById('content'));
