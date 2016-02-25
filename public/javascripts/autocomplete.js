f78072312ffe97eee52202fb76092f78e8d3fd42
Shariq Aziz <shariqazz@dhcp-206-87-198-68.ubcsecure.wireless.ubc.ca>
2016-02-24 16:26:23 -0800

    autocomplete started in search

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -25,3 +25,34 @@ $(function() {
             }
     });
 });
+
+// WITH FILTER - STILL WORKING ON THIS
+// $(function() {
+//     $('#auto').autocomplete({
+//             source: function(req, res) {
+//                 $.ajax({
+//                   url: '/allusers',
+//                   dataType: 'json',
+//                   cache: false,
+//                   success: function(data) {
+//                     console.log("SUCCESS!!!!!!!");
+//                     console.log(data);
+//                     $.grep(data, function(item){
+//                       return item.fullname.substring(0, req.term.length).toLowerCase() === req.term.toLowerCase();
+//                     });
+//                     res($.map(data, function(item){
+//                       return {
+//                         label: item.fullname,
+//                         value: item.id
+//                       };
+//                     });
+//                     );
+//                   },
+//                   error: function(xhr) {
+//                     console.log("ERROR WTF!!!");
+//                     console.log(xhr.status);
+//                   }
+//                 });
+//             }
+//     });
+// });
