ba9fa9f0b8942d6d5b77ba0966bcb3a73a34ee5f
lisa <li.mw.lisa@gmail.com>
2016-05-24 15:51:09 -0700

    playing around with APIs

--- public/javascripts/import.js
+++ public/javascripts/import.js
@@ -9,25 +9,29 @@ var ImportContent = React.createClass({
      	results: {}
         };
     },
-	queryCrossRef: function(e) {
+	querySciDir: function(e) {
 		e.preventDefault();
 
-		var nameQuery = this.state.name; // TODO split etc
+		var nameQuery = this.state.name || 'saeed ghafghazi'; // TODO split etc
 		this.setState({createStatus: 'Please wait...'});
 
 
 		$.ajax({
-			url: 'http://api.crossref.org/works?query=' + 'allen+renear' + '',
-			type: 'GET',
+			url: 'http://api.elsevier.com/content/search/scidir?apiKey=TODO&query=specific-author(' + nameQuery + ')&query=srctype(serial)&view=COMPLETE',
+			dataType: 'jsonp',
+			headers: {
+				'X-ELS-APIKey': 'TODO',
+				'Accept':'application/json'
+			},
 			success: function(data) {
 				console.log(data);
-				var totalResults = data.message['total-results'];
-				var entry = data.message.items;
+				var totalResults = data['search-results']['opensearch:totalResults'];
+				var entry = data['search-results']['entry'];
 				console.log(JSON.stringify(entry,null,4));
 				// TODO: displsy
 			}.bind(this),
 			error: function(xhr, status, err) {
-				console.error('http://api.crossref.org/works/', status, err.toString());
+				console.error('http://api.elsevier.com', status, err, xhr);
 				this.setState({ autoFillStatus: "DOI not found. Try again." });
 			}.bind(this)
 		});
@@ -35,11 +39,11 @@ var ImportContent = React.createClass({
 	render: function() {
 		var content;
 		if (this.state.status == 'askForAction') {
-			content = <ImportButtons />
+			content = <ImportButtons querySciDir={this.querySciDir} />
 		} else if (this.state.status == 'searching') {
-			content = <div>Searching CrossRef...</div>
+			content = <div>Searching ScienceDirect...</div>
 		} else {
-			content = <ResultsTable queryCrossRef=/>
+			content = <ResultsTable querySciDir={this.querySciDir} />
 		}
 
 		return (
@@ -51,10 +55,17 @@ var ImportContent = React.createClass({
 });
 
 var ImportButtons = React.createClass({
+    querySciDir(e) {
+        console.log('here');
+        // this.setMsg('In progress...');
+        this.props.querySciDir(e);
+        console.log("end");
+    },
+
 	render: function() {
 		return (
 			<div id="">
-				<Button className="btn-success btn-lg" onClick={this.queryCrossRef}>Yes, import my works</Button>
+				<Button className="btn-success btn-lg" onClick={this.querySciDir}>Yes, import my works</Button>
 				<Button className="btn-secondary btn-lg space" onClick={this.redirect}>No, proceed to Syncholar</Button>
 			</div>
 		);
