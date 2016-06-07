7566b7fad0e4919f50c38cc48192120f4e96e5fc
Shariq Aziz <shariqazz@dhcp-206-87-198-253.ubcsecure.wireless.ubc.ca>
2016-06-07 14:28:18 -0700

    search page stubs in place

--- public/javascripts/searchComponents.js
+++ public/javascripts/searchComponents.js
@@ -6,10 +6,10 @@
 //var Router = require('react-router').Router
 //var Route = require('react-router').Route
 //var Link = require('react-router').Link
-var isNode = typeof require !== 'undefined'
-var React = typeof require === 'undefined'
-             ? window.React
-             : require('react')
+// var isNode = typeof require !== 'undefined'
+// var React = typeof require === 'undefined'
+//              ? window.React
+//              : require('react')
 
 var SearchFeed = React.createClass({
   getInitialState: function() {
@@ -57,311 +57,66 @@ var SearchFeed = React.createClass({
           console.log("OBJ IS ====>>>");
           console.log(obj["about"]);
           switch (obj["type"]) {
-          case "user":
-            usersFound = true;
-            link = "/profile/" + key;
-            users.push({
-              "obj": obj,
-              "link": link,
-              "key" : key
-            });
-            break;
-          case "model":
-            modelsFound = true;
-            link = "/model/"  + obj["id"];
-            models.push({
-              "obj": obj,
-              "link": link,
-              "key" : key
-            });
-            break;
-          case "data":
-            dataFound = true;
-            link = "/data/" + obj["id"];
-            data.push({
-              "obj": obj,
-              "link": link,
-              "key": key
-            });
-            break;
-          case "publication":
-            pubsFound = true;
-            link = "/publication/" + obj["id"];
-            publications.push({
-              "obj": obj,
-              "link": link,
-              "key": key
-            });
-            break;
-          case "organization":
-            orgsFound = true;
-            link = "/organization/" + obj["id"];
-            organizations.push({
-              "obj": obj,
-              "link": link,
-              "key": key
-            });
-            break;
-          case "default":
-            link = "/";
+            case "user":
+              usersFound = true;
+              link = "/profile/" + key;
+              users.push({
+                "obj": obj,
+                "link": link,
+                "key" : key
+              });
+              break;
+            case "model":
+              modelsFound = true;
+              link = "/model/"  + obj["id"];
+              models.push({
+                "obj": obj,
+                "link": link,
+                "key" : key
+              });
+              break;
+            case "data":
+              dataFound = true;
+              link = "/data/" + obj["id"];
+              data.push({
+                "obj": obj,
+                "link": link,
+                "key": key
+              });
+              break;
+            case "publication":
+              pubsFound = true;
+              link = "/publication/" + obj["id"];
+              publications.push({
+                "obj": obj,
+                "link": link,
+                "key": key
+              });
+              break;
+            case "organization":
+              orgsFound = true;
+              link = "/organization/" + obj["id"];
+              organizations.push({
+                "obj": obj,
+                "link": link,
+                "key": key
+              });
+              break;
+            case "default":
+              link = "/";
             break;
         };
     }});
 
     return (
-        React.createElement("div", {},
-
-          React.createElement(
-    "nav",
-    { className: "navbar navbar-default" },
-    React.createElement(
-        "div",
-        { className: "container-fluid", style: {"max-width":"1180px"}, style: {"padding-left":"30px"}, style: {margin:"auto"} },
-        React.createElement(
-            "div",
-            { className: "navbar-header" },
-            React.createElement(
-                "button",
-                { type: "button", "data-toggle": "collapse", "data-target": "#bs-example-navbar-collapse-1", "aria-expanded": "false", className: "navbar-toggle collapsed" },
-                React.createElement(
-                    "span",
-                    { className: "sr-only" },
-                    "Toggle Navigation"
-                ),
-                React.createElement("span", { className: "icon-bar" }),
-                React.createElement("span", { className: "icon-bar" }),
-                React.createElement("span", { className: "icon-bar" })
-            ),
-            React.createElement(
-                "a",
-                { href: "/newsfeed", className: "navbar-brand" },
-                React.createElement("img", { alt: "RT", src: "/images/beaker-invert.png", width: "30" })
-            )
-        ),
-        React.createElement(
-            "div",
-            { id: "bs-example-navbar-collapse-1", className: "collapse navbar-collapse" },
-            React.createElement(
-                "ul",
-                { className: "nav navbar-nav" },
-                React.createElement(
-                    "li",
-                    { className: "dropdown" },
-                    React.createElement(
-                        "a",
-                        { href: "#", className: "dropdown-toggle", "data-toggle": "dropdown", role: "button", "aria-haspopup": "true", "aria-expanded": "false" },
-                        React.createElement("span", { className: "glyphicon glyphicon-menu-hamburger", "aria-hidden": "true" }),
-                        " Menu ",
-                        React.createElement("span", { className: "caret" })
-                    ),
-                    React.createElement(
-                        "ul",
-                        { className: "dropdown-menu" },
-                        React.createElement(
-                            "li",
-                            null,
-                            React.createElement(
-                                "a",
-                                { href: "/create/organization" },
-                                "Create Organization"
-                            )
-                        ),
-                        React.createElement(
-                            "li",
-                            null,
-                            React.createElement(
-                                "a",
-                                { href: "/report" },
-                                "Report Problem"
-                            )
-                        )
-                    )
-                )
-            ),
-            React.createElement(
-                "ul",
-                { className: "nav navbar-nav navbar-right" },
-                React.createElement(
-                    "li",
-                    { className: "dropdown" },
-                    React.createElement(
-                        "a",
-                        { href: "#", className: "dropdown-toggle", "data-toggle": "dropdown", role: "button", "aria-haspopup": "true", "aria-expanded": "false" },
-                        React.createElement(
-                            "span",
-                            { className: "logout-button" },
-                            React.createElement("i", { className: "fa fa-user-plus" })
-                        ),
-                        React.createElement("span", { id: "notification-request" })
-                    ),
-                    React.createElement("ul", { id: "friendrequest", className: "dropdown-menu", "aria-labelledby": "dropdownMenuDivider" })
-                ),
-                React.createElement(
-                    "li",
-                    { className: "dropdown" },
-                    React.createElement(
-                        "a",
-                        { href: "/signout", role: "button", className: "logout-button" },
-                        React.createElement("span", { className: "glyphicon glyphicon-log-out body-link" })
-                    )
-                )
-            ),
-            React.createElement(
-                "form",
-                { role: "search", method: "post", action: "/searchpage", className: "navbar-form navbar-right" },
-                React.createElement(
-                    "div",
-                    { className: "form-group form-inline", style: {width:"100%"} },
-                    React.createElement(
-                        "div",
-                        { className: "input-group purple", style: {width:"100%"} },
-                        React.createElement("input", { id: "message", type: "text", name: "searchQuery", placeholder: "Search...", className: "form-control search-bar auto" }),
-                        React.createElement(
-                            "button",
-                            { type: "submit", className: "btn btn-primary btn-search searchButton", disabled: "disabled" },
-                            React.createElement("span", { "aria-hidden": "true", className: "glyphicon glyphicon-search" })
-                        )
-                    )
-                )
-            )
-        )
+      <div> SEARCH PAGE STUFF </div>
     )
-),
-
-          React.createElement("h2", {className: "text-center"}, "Search Results"),
-          React.createElement("h3", {className: "leftClass text-center", style: {display: usersFound?"":"none"}}, "Users"),
-          React.createElement("div", {className: "", style: {display: usersFound?"inline":"none"}},
-            React.createElement("div", {className: "row"},
-              React.createElement("ul", {}, 
-                React.createElement("div", {className: "list-group"}, users.map(function(i){
-                  obj = i["obj"]
-                  link = i["link"]
-                  key = i["key"]
-                  return(
-                    //React.createElement("li", {},
-                      React.createElement("a", {href: link, className: "list-group-item"},
-                        React.createElement("div", {className: "clearfix"},
-                          React.createElement("img", {src: obj["img"], height: "120px", width: "120px", style: {}, className: "pull-left gap-all img-circle"}),
-                          React.createElement("h4", {className: "list-group-item-heading", style: {color: "black"}}, key)
-                        ),
-                        React.createElement("p", {className: "list-group-item-text", style: {color: "black"}}, obj["about"])
-                      )
-                      // key)
-                    //)
-                  )
-                  })
-                )
-              )
-            )
-          ),
-          React.createElement("h3", {className: "leftClass text-center", style: {display: dataFound?"":"none"}}, "Data"),
-          React.createElement("div", {className: "", style: {display: dataFound?"inline":"none"}},
-            React.createElement("div", {className: "row"},
-              React.createElement("ul", {}, 
-                React.createElement("div", {className: "list-group"}, data.map(function(i){
-                  obj = i["obj"]
-                  link = i["link"]
-                  key = i["key"]
-                  return(
-                    //React.createElement("li", {},
-                      React.createElement("a", {href: link, className: "list-group-item"},
-                        React.createElement("div", {className: "clearfix"},
-                          React.createElement("img", {src: obj["img"], height: "120px", width: "120px", style: {}, className: "pull-left gap-all img-circle"}),
-                          React.createElement("h4", {className: "list-group-item-heading", style: {color: "black"}}, key)
-                        ),
-                        React.createElement("p", {className: "list-group-item-text", style: {color: "black"}}, obj["about"])
-                      )
-                    //)
-                  )
-                  })
-                )
-              )
-            )
-          ),
-          React.createElement("h3", {className: "leftClass text-center", style: {display: pubsFound?"":"none"}}, "Publications"),
-          React.createElement("div", {className: "", style: {display: pubsFound?"inline":"none"}},
-            React.createElement("div", {className: "row"},
-              React.createElement("ul", {}, 
-                React.createElement("div", {className: "list-group"}, publications.map(function(i){
-                  obj = i["obj"]
-                  link = i["link"]
-                  key = i["key"]
-                  return(
-                    //React.createElement("li", {},
-                      React.createElement("a", {href: link, className: "list-group-item"},
-                        React.createElement("div", {className: "clearfix"},
-                          React.createElement("img", {src: obj["img"], height: "120px", width: "120px", style: {}, className: "pull-left gap-all img-circle"}),
-                          React.createElement("h4", {className: "list-group-item-heading", style: {color: "black"}}, key)
-                        ),
-                        React.createElement("p", {className: "list-group-item-text", style: {color: "black"}}, obj["about"])
-                      )
-                    //)
-                  )
-                  })
-                )
-              )
-            )
-          ),
-          React.createElement("h3", {className: "leftClass text-center", style: {display: orgsFound?"":"none"}}, "Organizations"),
-          React.createElement("div", {className: "", style: {display: orgsFound?"inline":"none"}},
-            React.createElement("div", {className: "row"},
-              React.createElement("ul", {}, 
-                React.createElement("div", {className: "list-group"}, organizations.map(function(i){
-                  obj = i["obj"]
-                  link = i["link"]
-                  key = i["key"]
-                  return(
-                    //React.createElement("li", {},
-                      React.createElement("a", {href: link, className: "list-group-item"},
-                        React.createElement("div", {className: "clearfix"},
-                          React.createElement("img", {src: obj["img"], height: "120px", width: "120px", style: {}, className: "pull-left gap-all img-circle"}),
-                          React.createElement("h4", {className: "list-group-item-heading", style: {color: "black"}}, key)
-                        ),
-                        React.createElement("p", {className: "list-group-item-text", style: {color: "black"}}, obj["about"])
-                      )
-                    //)
-                  )
-                  })
-                )
-              )
-            )
-          ),
-          React.createElement("h3", {className: "leftClass text-center", style: {display: modelsFound?"":"none"}}, "Model"),
-          React.createElement("div", {className: "", style: {display: modelsFound?"inline":"none"}},
-            React.createElement("div", {className: "row"},
-              React.createElement("ul", {}, 
-                React.createElement("div", {className: "list-group"}, models.map(function(i){
-                  obj = i["obj"]
-                  link = i["link"]
-                  key = i["key"]
-                  return(
-                    //React.createElement("li", {},
-                      React.createElement("a", {href: link, className: "list-group-item"},
-                        React.createElement("div", {className: "clearfix"},
-                          React.createElement("img", {src: obj["img"], height: "120px", width: "120px", style: {}, className: "pull-left gap-all img-circle"}),
-                          React.createElement("h4", {className: "list-group-item-heading", style: {color: "black"}}, key)
-                        ),
-                        React.createElement("p", {className: "list-group-item-text", style: {color: "black"}}, obj["about"])
-                      )
-                    //)
-                  )
-                  })
-                )
-              )
-            )
-          )
-        )
-      )
-    }
+  }
 });
 
- if (isNode) {
-   exports.SearchFeed = SearchFeed;
- } else {
   $( document ).ready(function() {
-    ReactDOM.render(React.createElement(SearchFeed, {}), document.getElementById('reactSearchContainer'));
+    ReactDOM.render(React.createElement(SearchFeed, {}), document.getElementById('content'));
   });
- }
 
 
 
---
+++ routes/search.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var formidable = require('formidable');
+var util = require('util');
+var fs  = require('fs-extra');
+var moment = require('moment');
+var path = require('path');
+var _= require('underscore');
+var aws = require('aws-sdk');
+var s3 = new aws.S3();
+var awsUtils = require('../utils/awsUtils');
+var awsLink = "https://s3-us-west-2.amazonaws.com/syncholar/";
+var is_auth = require('../utils/helpers').is_auth;
+
+module.exports = function(app, Parse, io) {
+	app.get('/search', function(req, res, next) {
+		res.render('search', {});
+	});
+}
