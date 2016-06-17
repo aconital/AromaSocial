ec859a842cfaa915f05f69515fb9d86ae313a0ea
Shariq Aziz <shariqazz@dhcp-206-87-199-149.ubcsecure.wireless.ubc.ca>
2016-06-17 15:51:42 -0700

    navbar user undefined issue fixed

--- routes/search.js
+++ routes/search.js
@@ -10,10 +10,16 @@ var s3 = new aws.S3();
 var awsUtils = require('../utils/awsUtils');
 var awsLink = "https://s3-us-west-2.amazonaws.com/syncholar/";
 var is_auth = require('../utils/helpers').is_auth;
+var include_user= require('../utils/helpers').include_user;
 
 module.exports = function(app, Parse, io) {
-	app.get('/search', function(req, res, next) {
-		var searchString = req.query.searchQuery;
-		res.render('search', {searchString: searchString});
+	app.get('/search', include_user, function(req, res, next) {
+		console.log(req);
+		if (req.isAuthenticated()) {
+			var searchString = req.query.searchQuery;
+			res.render('search', {searchString: searchString});
+		} else {
+			res.json("Not signed in");
+		}
 	});
 }
