8a0acf0cd12e9bcb963f0126b474d3f135487220
Shariq Aziz <shariqazz@Shariqs-MacBook-Pro.local>
2016-07-26 11:36:09 -0700

    invite modal changed - testing

--- public/javascripts/invitation.js
+++ public/javascripts/invitation.js
@@ -16,15 +16,13 @@ var InviteModal = React.createClass({
                 <div className="col-xs-12">
                     <form>
                         <label>Title</label>
-                        < ReactMultiSelect />
-                        <input>
+                        <ReactMultiSelect />
+                        <input
                             placeholder="Enter email"
-                            className="creatediscussion-topic"
-                            type="text"
+                            type="text" >
                         </input>
                         <textarea
-                         rows="8" cols="50"
-                         className="creatediscussion-input" >
+                         rows="5" cols="50" >
                         </textarea>
                         <button type="submit" value="Submit"></button>
                     </form>
@@ -37,7 +35,7 @@ var InviteModal = React.createClass({
 var ModalButton = React.createClass({
 	getInitialState: function() {
 		return {
-			showModal: false
+			showModal: true
 		}
 	},
 	closeModal: function() {
@@ -48,6 +46,11 @@ var ModalButton = React.createClass({
 	},
 	render: function() {
 		return (
+			<div>
+			<Button onClick={this.openModal}>
+	          Launch modal
+	        </Button>
+
 			<Modal show={this.state.showModal} onHide={this.closeModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>Invite a Friend</Modal.Title>
@@ -56,22 +59,28 @@ var ModalButton = React.createClass({
                        <InviteModal />
                 </Modal.Body>
             </Modal>
+            </div>
 		)
 	}
 });
 
+
 var DropDown = React.createClass({
 	render: function() {
 		return (
-            <ul className="dropdown-menu">
-                <li><a href="/create/organization">Create Organizational Homepage</a></li>
-                <li><a href="/report">Report Problem | Share Idea</a></li>
-                <li><ModalButton /></li>
-            </ul>
+            <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                    <span className="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span> Menu <span className="caret"></span>
+                </a>
+                <ul className="dropdown-menu">
+                    <li><a href="/create/organization">Create Organizational Homepage</a></li>
+                    <li><a href="/report">Report Problem | Share Idea</a></li>
+                </ul>
+            </li>
 		)
 	}
 });
 ReactDOM.render(
-  <DropDown />,
-  document.getElementById('mainDropdown')
+  <ModalButton />,
+  document.getElementById('testDiv')
 );
